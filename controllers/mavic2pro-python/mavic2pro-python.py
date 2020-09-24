b68d1457301720e2bbf0e127cdf202f1bb7ce4ea
Benjamin Davey <bldavey@deakin.edu.au>
2020-09-23 23:03:35 -0700

    Clean mavic2pro-python.py file

--- controllers/mavic2pro-python/mavic2pro-python.py
+++ controllers/mavic2pro-python/mavic2pro-python.py
@@ -1,13 +1,14 @@
-"""mavic2pro controller."""
+# mavic2pro controller
+# rewritten in Python from the original C sample source from webots (which is licensed under Apache 2.0)
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
+# Import classes
 from controller import Robot
 from controller import InertialUnit
 from controller import Gyro
 from controller import Motor
 import math
 
+#Function definitions
 def clamp(n, smallest, largest): 
     return max(smallest, min(n, largest))
 
@@ -17,15 +18,11 @@ delivery_drone = Robot()
 # get the time step of the current world.
 timestep = int(delivery_drone.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
+# Initialise devices
 imu = delivery_drone.getInertialUnit('inertial unit')
-gyro = delivery_drone.getGyro('gyro')
-
 imu.enable(timestep)
+
+gyro = delivery_drone.getGyro('gyro')
 gyro.enable(timestep)
 
 front_left_motor = delivery_drone.getMotor("front left propeller")
@@ -36,7 +33,8 @@ motors = [front_left_motor, front_right_motor, rear_left_motor, rear_right_motor
 for motor in motors:
     motor.setPosition(float('inf'))
     motor.setVelocity(1.0)
-      
+
+# Balance variables
 k_vertical_thrust = 68.5;  # with this thrust, the drone lifts.
 k_vertical_offset = 0.6;   # Vertical offset where the robot actually targets to stabilize itself.
 k_vertical_p = 3.0;        # P constant of the vertical PID.
@@ -46,12 +44,8 @@ k_pitch_p = 30.0;          # P constant of the pitch PID.
 target_altitude = 1.0
 
 # Main loop:
-# - perform simulation steps until Webots is stopping the controller
 while delivery_drone.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-    
+    # SENSE
     time = delivery_drone.getTime()
     
     imu_roll_pitch_yaw = imu.getRollPitchYaw()
@@ -64,22 +58,22 @@ while delivery_drone.step(timestep) != -1:
     roll_acceleration = gyro_x_y_z[0]
     pitch_acceleration = gyro_x_y_z[1]
     
+    # These variables will be used to affect movement in X & Z
     roll_disturbance = 0.0;
     pitch_disturbance = 0.0;
     yaw_disturbance = 0.0;
     
-    altitude = 0.9
+    # This is where the robot thinks it is in Y
+    altitude = 1.0
 
-    # Process sensor data here.
-    
+    # THINK
     roll_input = k_roll_p * clamp(roll, -1.0, 1.0) + roll_acceleration + roll_disturbance
     pitch_input = k_pitch_p * clamp(pitch, -1.0, 1.0) - pitch_acceleration + pitch_disturbance
     yaw_input = yaw_disturbance
     clamped_difference_altitude = clamp(target_altitude - altitude + k_vertical_offset, -1.0, 1.0)
     vertical_input = k_vertical_p * pow(clamped_difference_altitude, 3.0)
 
-    # Actuate the motors taking into consideration all the computed inputs.
-    
+    # ACT
     front_left_motor_input = k_vertical_thrust + vertical_input - roll_input - pitch_input + yaw_input;
     front_right_motor_input = k_vertical_thrust + vertical_input + roll_input - pitch_input - yaw_input;
     rear_left_motor_input = k_vertical_thrust + vertical_input - roll_input + pitch_input - yaw_input;
@@ -91,9 +85,6 @@ while delivery_drone.step(timestep) != -1:
     rear_right_motor.setVelocity(rear_right_motor_input)
     
     pass
-
-# Enter here exit cleanup code.
-
 
 
 
